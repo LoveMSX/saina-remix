859bc69c984da16ac2951b56666d7ddda643bef4
Emmanuel <em.razanakoto@gmail.com>
2025-09-30 11:42:10 +0300

    setup env

--- src/main.tsx
+++ src/main.tsx
@@ -6,11 +6,9 @@ import AuthProvider from './context/user/index.tsx'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 import './index.css'
 
-const CLIENT_GOOGLE_ID = '403752291305-0lhscpeo65p7h709b6le88nopvkup5io.apps.googleusercontent.com'
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <GoogleOAuthProvider clientId={CLIENT_GOOGLE_ID}>
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_CLIENT_GOOGLE_ID}>
       <AuthProvider>
         <MainRouter/>
       </AuthProvider>
